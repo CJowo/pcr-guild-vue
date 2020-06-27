d9c66c01894c0bc46cc12a1617599128434ba2d1
CJowo <2315903293@qq.com>
2020-06-28 01:07:19 +0800

    公会box卡牌显示优化

--- src/pages/Guild/components/GuildDetailBoxCard.vue
+++ src/pages/Guild/components/GuildDetailBoxCard.vue
@@ -1,20 +1,20 @@
 <template>
   <q-card class="q-mt-md">
     <q-card-section class="row justify-between">
-      <span class="text-h6">
-        <q-toggle
-          v-model="detail"
-          color="primary"
-        />
-      </span>
       <q-chip outline color="primary" text-color="white" icon="account_circle" style="cursor: pointer" >
         {{ item.nickname }}
         <q-popup-proxy>
             <UserCard :user="item"/>
         </q-popup-proxy>
       </q-chip>
+      <span class="text-h6">
+        <q-toggle
+          v-model="detail"
+          color="primary"
+        />
+      </span>
     </q-card-section>
-    <q-card-section class="row q-gutter-xs items-start">
+    <q-card-section class="row q-gutter-xs items-start" @click="detail=true">
       <Character
         v-for="character in item.characters"
         :key="character.name" :name="character.name"
@@ -29,10 +29,12 @@
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
 import Character from 'components/Character.vue'
+import UserCard from 'components/UserCard.vue'
 
 @Component({
   components: {
-    Character
+    Character,
+    UserCard
   }
 })
 export default class GuildDetailBoxCard extends Vue {
